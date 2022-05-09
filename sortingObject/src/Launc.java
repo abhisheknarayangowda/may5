04f40f81dacad2ba6299b57a98111bc43e7f7cee
Admin <Admin@DESKTOP-9833F0D>
2022-05-09 19:45:51 +0530

    1st commit of may5

---
+++ sortingObject/src/Launc.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Concurrent.CopyOnWriteArrayList;
+
+public class Launc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList = new ArrayList();
+		x.add(100);
+		x.add(150);
+		x.add(50);
+		x.add(25);
+		x.add(125);
+		x.add(175);
+		System.out.println(x);
+			Collections.sort(x);
+			System.out.println(x);
+	}
+
+}
